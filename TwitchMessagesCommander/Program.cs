8de1405492f40c88e41c83206ddf96d25c16e2f4
LewandowskiAnthony <lewandowski.anthony.pro@gmail.com>
2021-04-06 17:13:01 +0200

    Mise en place du systeme de threading

--- TwitchMessagesCommander/Program.cs
+++ TwitchMessagesCommander/Program.cs
@@ -14,8 +14,15 @@ using WindowsInput;
 
 namespace TwitchMessagesCommander
 {
+
     class Program
     {
+
+        private static Mutex _pool = new Mutex();
+        private static Mutex threadingLaunch = new Mutex();
+
+        private static Queue<String> commandsList = new Queue<String>();
+
         [DllImport("user32.dll")]
         static extern int SetForegroundWindow(IntPtr point);
         [STAThread]
@@ -26,21 +33,12 @@ namespace TwitchMessagesCommander
             //"SRC\\ROMS\\PokemonR.gb"
             //Process.Start(".\\SRC\\sameboy.exe");
             Process[] tousLesProcess = Process.GetProcesses();
-            String command;
-
-            // Socket Listening
-            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            socket.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 3000));
-            socket.Listen(0);
-            var client = socket.Accept();
-            var buffer = Encoding.UTF8.GetBytes("Hello NODEJS");
-            client.Send(buffer, 0, buffer.Length, 0);
-
+            String commandToExecute="";
+           
             foreach (Process Processus in tousLesProcess)
             {
                //int iDProcess = Processus.Id;
                String name = Processus.ProcessName;
-               Console.WriteLine("Le processus {0} :", name);
                 if (name == "bgb")
                {
                     Console.WriteLine("Le processus {0} est lancé", name);
@@ -49,76 +47,98 @@ namespace TwitchMessagesCommander
                     IntPtr h = p.MainWindowHandle;
                     Console.WriteLine("On envois l'appuie sur la touche entrée.");
                     InputSimulator sim = new InputSimulator();
+                    Thread t = new Thread(new ThreadStart(GetCommands));
+                    t.Start();
+                    Thread.Sleep(2000);
+                    threadingLaunch.WaitOne();
+                    SetForegroundWindow(h);
 
                     while (true)
                     {
-                        SetForegroundWindow(h);
-                        Thread.Sleep(2000);
-                        buffer = new byte[255];
-                        int rec = client.Receive(buffer, 0, buffer.Length, 0);
-                        Array.Resize(ref buffer, rec);
-                        command = Encoding.UTF8.GetString(buffer).ToLower();
-                        SetForegroundWindow(h);
-                        Console.WriteLine(command + " received");
-                        switch (command)
+                        threadingLaunch.WaitOne();
+                        if (commandsList.Count>0)
                         {
-                            case "a":
-                                sim.Keyboard.KeyDown(VirtualKeyCode.VK_A);
-                                Thread.Sleep(175);
-                                sim.Keyboard.KeyUp(VirtualKeyCode.VK_A);
-                                break;
-                            case "b":
-                                sim.Keyboard.KeyDown(VirtualKeyCode.VK_Z);
-                                Thread.Sleep(175);
-                                sim.Keyboard.KeyUp(VirtualKeyCode.VK_Z);
-                                break;
-                            case "start":
-                                sim.Keyboard.KeyDown(VirtualKeyCode.RETURN);
-                                Thread.Sleep(175);
-                                sim.Keyboard.KeyUp(VirtualKeyCode.RETURN);
-                                break;
-                            case "select":
-                                sim.Keyboard.KeyDown(VirtualKeyCode.SHIFT);
-                                Thread.Sleep(175);
-                                sim.Keyboard.KeyUp(VirtualKeyCode.SHIFT);
-                                break;
-                            case "up":
-                                sim.Keyboard.KeyDown(VirtualKeyCode.UP);
-                                Thread.Sleep(175);
-                                sim.Keyboard.KeyUp(VirtualKeyCode.UP);
-                                break;
-                            case "down":
-                                sim.Keyboard.KeyDown(VirtualKeyCode.DOWN);
-                                Thread.Sleep(175);
-                                sim.Keyboard.KeyUp(VirtualKeyCode.DOWN);
-                                break;
-                            case "left":
-                                sim.Keyboard.KeyDown(VirtualKeyCode.LEFT); 
-                                Thread.Sleep(175);
-                                sim.Keyboard.KeyUp(VirtualKeyCode.LEFT);
-                                break;
-                            case "right":
-                                sim.Keyboard.KeyDown(VirtualKeyCode.RIGHT);
-                                Thread.Sleep(175);
-                                sim.Keyboard.KeyUp(VirtualKeyCode.RIGHT);
-                                break;
-                            default:
-                                Console.WriteLine("No command available");
-                                break;
+                            _pool.WaitOne();
+                                commandToExecute = commandsList.Dequeue();
+                            _pool.ReleaseMutex();
+                            switch (commandToExecute)
+                            {
+                                case "a":
+                                    sim.Keyboard.KeyDown(VirtualKeyCode.VK_A);
+                                    Thread.Sleep(175);
+                                    sim.Keyboard.KeyUp(VirtualKeyCode.VK_A);
+                                    break;
+                                case "b":
+                                    sim.Keyboard.KeyDown(VirtualKeyCode.VK_Z);
+                                    Thread.Sleep(175);
+                                    sim.Keyboard.KeyUp(VirtualKeyCode.VK_Z);
+                                    break;
+                                case "start":
+                                    sim.Keyboard.KeyDown(VirtualKeyCode.RETURN);
+                                    Thread.Sleep(175);
+                                    sim.Keyboard.KeyUp(VirtualKeyCode.RETURN);
+                                    break;
+                                case "select":
+                                    sim.Keyboard.KeyDown(VirtualKeyCode.SHIFT);
+                                    Thread.Sleep(175);
+                                    sim.Keyboard.KeyUp(VirtualKeyCode.SHIFT);
+                                    break;
+                                case "up":
+                                    sim.Keyboard.KeyDown(VirtualKeyCode.UP);
+                                    Thread.Sleep(175);
+                                    sim.Keyboard.KeyUp(VirtualKeyCode.UP);
+                                    break;
+                                case "down":
+                                    sim.Keyboard.KeyDown(VirtualKeyCode.DOWN);
+                                    Thread.Sleep(175);
+                                    sim.Keyboard.KeyUp(VirtualKeyCode.DOWN);
+                                    break;
+                                case "left":
+                                    sim.Keyboard.KeyDown(VirtualKeyCode.LEFT);
+                                    Thread.Sleep(175);
+                                    sim.Keyboard.KeyUp(VirtualKeyCode.LEFT);
+                                    break;
+                                case "right":
+                                    sim.Keyboard.KeyDown(VirtualKeyCode.RIGHT);
+                                    Thread.Sleep(175);
+                                    sim.Keyboard.KeyUp(VirtualKeyCode.RIGHT);
+                                    break;
+                                default:
+                                    break;
+                            }
                         }
                     }
                 }
             }
             Console.WriteLine("End reached ;)");
 
-
-            client.Close();
-            socket.Close();
         }
 
         static void GetCommands()
         {
-            
+            String command;
+            threadingLaunch.WaitOne();
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 3000));
+            socket.Listen(0);
+            var client = socket.Accept();
+            var buffer = Encoding.UTF8.GetBytes("Hello NODEJS");
+            client.Send(buffer, 0, buffer.Length, 0);
+            threadingLaunch.ReleaseMutex();
+            while (true)
+            {
+                buffer = new byte[255];
+                int rec = client.Receive(buffer, 0, buffer.Length, 0);
+                Array.Resize(ref buffer, rec);
+                command = Encoding.UTF8.GetString(buffer).ToLower();
+                Console.WriteLine("{0} is requesting the mutex", Thread.CurrentThread.Name);
+                _pool.WaitOne();
+                    commandsList.Enqueue(command);
+                _pool.ReleaseMutex();
+                Console.WriteLine(command + " received");
+            }
+            client.Close();
+            socket.Close();
         }
     }
 }
